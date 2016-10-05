53d70334db2715767fdd1eef122b8767d825e721
ZHAO KAI <zeakey@126.com>
2016-10-05 15:06:17 +0800

    py-denp -> py-dep

--- py-dep.sh
+++ py-dep.sh
@@ -8,7 +8,7 @@ echo '#numpy include path' >> ~/.bashrc
 echo 'export CPP_INCLUDE_PATH=/usr/local/lib/python2.7/dist-packages/numpy/core/include:$CPP_INCLUDE_PATH' >> ~/.bashrc
 echo 'export C_INCLUDE_PATH=/usr/local/lib/python2.7/dist-packages/numpy/core/include:$C_INCLUDE_PATH' >> ~/.bashrc
 
-sudo -H pip install scikit-image scikit-learn 
+sudo -H pip install scikit-image scikit-learn
 sudo -H pip install matplotlib 
 #sudo pip install pyopencv
 sudo -H pip install ipython jupyter
@@ -19,5 +19,6 @@ sudo -H pip install pandas
 sudo -H pip install python-dateutil
 sudo -H pip install python-gflags pyyaml Pillow six pyzmq singledispatch
 sudo -H pip install backports_abc certifi jsonschema graphviz  qtawesome pydot
+sudo -H cp python/cv2.py /usr/local/lib/python2.7/dist-packages/
 
 source ~/.bashrc
